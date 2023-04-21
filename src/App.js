087490fdb4ce99900466901b7ecd65dce36dd43f
Nishthakumari <knishtha0@gmail.com>
2023-04-21 19:25:26 +0530

    Exploring React Props - Setting Counter increment value

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Counter from './components/counter/Counter';
 function App() {
   return (
     <div className="App">
-     <Counter/>
+     <Counter by="1"/>
+     <Counter by="2"/>
+     <Counter by="3"/>
     </div>
   );
 }

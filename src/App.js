fbfefe1831bfa6372083bc52ef076dca93ee2509
Nishthakumari <knishtha0@gmail.com>
2023-04-21 13:42:30 +0530

    Exploring JSX - React Views

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       My Todo Application
-      <FirstComponent></FirstComponent>
-      <SecondComponent></SecondComponent>
-      <ThirdComponent></ThirdComponent>
-      <FourthComponent></FourthComponent>
+      <FirstComponent/>
+      <SecondComponent/>
+      <ThirdComponent/>
+      <FourthComponent/>
     </div>
   );
 }
